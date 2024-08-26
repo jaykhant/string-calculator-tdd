d2e7d479daf1ffad13ec36520b053437294c6f73
Jay Khant <jay.khant.79@gmail.com>
2024-08-26 20:26:24 +0530

    Created the test for the sum of multiple numbers

--- calculator.test.js
+++ calculator.test.js
@@ -11,3 +11,7 @@ test('should return the number itself if there is only one number', () => {
 test('should return the sum of two numbers', () => {
     expect(calculator('1,2')).toBe(3)
 })
+
+test('should return the sum of multiple numbers', () => {
+    expect(calculator('1,2,3,4')).toBe(10)
+})
