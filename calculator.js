71c5d816193d8004173a6608f5705fd169716fc7
Jay Khant <jay.khant.79@gmail.com>
2024-08-26 20:35:43 +0530

    Implemented the code to handle custom delimiter

--- calculator.js
+++ calculator.js
@@ -3,6 +3,13 @@ const calculator = (input) => {
 
     const delimiters = [',', '\n']
 
+    if (input.startsWith('//')) {
+        const delimiterEndIndex = input.indexOf('\n')
+        const customDelimiter = input.substring(2, delimiterEndIndex)
+        delimiters.push(customDelimiter)
+        input = input.substring(delimiterEndIndex + 1)
+    }
+
     const numbers = input.split(new RegExp(`[${delimiters.join('')}]`))
 
     return numbers.reduce((sum, number) => sum + parseInt(number), 0)
