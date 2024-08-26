270a3fc8e9d78dacc9570b63fef5b5851c6f4c28
Jay Khant <jay.khant.79@gmail.com>
2024-08-26 20:20:39 +0530

    Created the test for single number

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator');
 test('should return 0 for an empty string', () => {
     expect(calculator('')).toBe(0)
 })
+
+test('should return the number itself if there is only one number', () => {
+    expect(calculator('5')).toBe(5)
+})
